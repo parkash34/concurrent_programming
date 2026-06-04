28233f779f6e6446ce182e1ae1f6cdb0651d442a
parkash34 <ohm.parkash34@gmail.com>
2026-06-04 17:16:06 +0200

    Updated it

--- week2/PrintThread.java
+++ week2/PrintThread.java
@@ -1,58 +1,42 @@
-// package week2;
-
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-
-class ChildThread extends Thread{
-    String str;
-
-    public ChildThread(String str){
-        this.str = str;
-    }
-    public void run(){
-        for(int i = 0; i < 10_000; i++){
-            System.out.println(str);
-        }
-    } 
-}
-
-public class PrintThread {
-        public static void main(String[] args) throws InterruptedException, IOException{
+public class PrintThread{
+    public static void main(String[] args) throws IOException, InterruptedException {   
         try(PrintWriter pw = new PrintWriter("out.txt")){
 
-            // Thread t1 = new ChildThread("Hello"),
-            // t2 = new ChildThread("world");
-
-            // Thread t1 = new Thread(){
-            //     public void run(){
-            //         for(int i = 0; i < 10_000; i++){
-            //             pw.println("Hello");
-            //         }
-            //     }
-            // }, t2 = new Thread(new Runnable(){
-            //     public void run(){
-            //         for(int i = 0; i < 10_000; i++){
-            //             pw.println("world");
-            //         }
-            //     }
-            // });
             Thread t1 = null, t2 = null;
             for(int i = 0; i < 2; i++){
-                String s = i == 0 ? "Hello" : "world";
+                String s = i == 0 ? "Hello" : "World";
                 Thread t = new Thread(() -> {
                     for(int j = 0; j < 10_000; j++){
+                        if(j != 0){
+                            try{
+                                Thread.sleep(5);
+                            } catch(InterruptedException e){
+                                // ignore
+                                Thread.currentThread().interrupt(); //re-interrupts
+                                break;
+                            }
+                        }
                         pw.println(s);
                     }
+                    pw.println("ready");
                 });
-                if(i == 0) t1 = t;
+                if (i == 0) t1 = t;
                 else t2 = t;
-            }
 
+            }
             t1.start();
             t2.start();
+
+            Thread.sleep(1000);
+            t1.interrupt();
+            t2.interrupt();
+
             t1.join();
             t2.join();
-        } // catch (Exception e){e.printStackTrace(); System.exit(-1);}
+            System.out.println();
+        }        
     }
 }
--- week3/PrintThread.java
+++ week3/PrintThread.java
@@ -1,33 +1,34 @@
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class PrintThread{
-        public static void main(String[] args) throws InterruptedException, IOException{
+    public static void main(String[] args) throws IOException, InterruptedException {
         Thread[] ts = new Thread[10];
-        System.out.println("Available Processors: " + Runtime.getRuntime().availableProcessors());
+        System.out.println("Available Processors : " + Runtime.getRuntime().availableProcessors());
         for(int i = 0; i < ts.length; i++){
-            // int fi = i;
             ts[i] = new Thread(() -> {
                 String name = Thread.currentThread().getName();
-                // String name = ts[fi].getName();
-                try(PrintWriter pw = new PrintWriter(name)){
-                    for(int j = 1; j <= 1_000_000; j++){
+                try(PrintWriter pw = new PrintWriter(name)) {
+                        for(int j = 1; j <= 1_000_000; j++){
                         pw.println(j);
                     }
-                } catch(IOException e){}
+                } catch(IOException e) {}
             });
         }
+
         long startTime = System.nanoTime();
         for(int i = 0; i < ts.length; i++){
             ts[i].start();
         }
-        Thread.sleep(50);
+
+        Thread.sleep(1000);
+
         for(int i = 0; i < ts.length; i++){
-            try(BufferedReader br = new BufferedReader(new FileReader(ts[i].getName()))){
+            try(BufferedReader br = new BufferedReader(new FileReader(ts[i].getName()))) {
                 String last = null, s = null;
-                while((s = br.readLine()) != null){
+                while((s = br.readLine()) != null) {
                     last = s;
                 }
                 System.out.println(last);
@@ -36,38 +37,43 @@ public class PrintThread{
         for(int i = 0; i < ts.length; i++){
             ts[i].join();
         }
-        System.out.println("Total time: " + (System.nanoTime() - startTime)*1e-9 + "s");
-        try(PrintWriter pw = new PrintWriter("out.txt")){
+
+        System.out.println("Total time: " + (System.nanoTime() - startTime) * 1e-9 + "s");
             
+        try(PrintWriter pw = new PrintWriter("out.txt")){
+
             Thread t1 = null, t2 = null;
             for(int i = 0; i < 2; i++){
-                String s = i == 0 ? "Hello" : "world";
+                String s = i == 0 ? "Hello" : "World";
                 Thread t = new Thread(() -> {
                     for(int j = 0; j < 10_000; j++){
                         if(j != 0){
                             try{
                                 Thread.sleep(5);
                             } catch(InterruptedException e){
-                                //ignore
+                                // ignore
                                 Thread.currentThread().interrupt(); //re-interrupts
-                                break; // exit
+                                break;
                             }
                         }
                         pw.println(s);
                     }
                     pw.println("ready");
                 });
-                if(i == 0) t1 = t;
+                if (i == 0) t1 = t;
                 else t2 = t;
-            }
 
+            }
             t1.start();
             t2.start();
+
             Thread.sleep(1000);
             t1.interrupt();
             t2.interrupt();
+
             t1.join();
             t2.join();
-        }
+            System.out.println();
+        }        
     }
 }
